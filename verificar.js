f065bbf956be5e30d81d5fd0bb0ec2a8448febdb
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-08-12 19:01:41 -0300

    ingreso implementado

--- verificar.js
+++ verificar.js
@@ -1,19 +1,38 @@
-const inputUsuario=document.querySelector("#usuario");
-const inputContrasenia=document.querySelector("#contrasenia");
+const inputUsuario = document.querySelector("#usuario");
+const inputContrasenia = document.querySelector("#contrasenia");
 
-const CLAVE_USUARIOS="lista-usuarios";
-const CLAVE_CONTRASENIAS="lista-contrasenias";
+const CLAVE_USUARIOS = "lista-usuarios";
+const CLAVE_CONTRASENIAS = "lista-contrasenias";
 
 function verificar() {
-    let nombre=inputUsuario.value;
-    let usuario=inputContrasenia.value;
-    let listaUsuarios= "def lista usuarios";
-    let listaContrasenias= "def lista contrasenias";
+    let nombre = inputUsuario.value;
+    let clave = inputContrasenia.value;
+    let listaUsuarios = "def lista usuarios";
+    let listaContrasenias = "def lista contrasenias";
+    let ingresoVerificado = false;
 
-    listaUsuarios= JSON.parse(localStorage.getItem(CLAVE_USUARIOS));
+    listaUsuarios = JSON.parse(localStorage.getItem(CLAVE_USUARIOS));
     if (listaUsuarios === null) {
-        alert ("no hay cuentas registradas");
+        alert("no hay cuentas registradas");
     } else {
-        
+
+        for (let i = 0; i < listaUsuarios.length; i++) {
+            if (listaUsuarios[i] === nombre) {
+                listaContrasenias = JSON.parse(localStorage.getItem(CLAVE_CONTRASENIAS));
+                /* alert("encontre el nombre del usuario"); */
+            }
+            for (let i = 0; i < listaContrasenias.length; i++) {
+                if (listaContrasenias[i] === clave) {
+                    ingresoVerificado = true;
+                    /*  alert("contraseña verificada"); */
+                }
+
+            }
+        }
+        if (ingresoVerificado) {
+            alert("cuenta verificada, puede ingresar")
+        } else {
+            alert("usuario y/o clave incorrecto")
+        }
     }
 }
